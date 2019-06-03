774166a179c85a4107ca9dadfe647bbe0f1b5bd3
FedorovVladimir <vladimir.fodorow@gmail.com>
2019-06-03 18:42:42 +0300

    Создание модуля теста

--- src/test/java/CreateModuleTest.java
+++ src/test/java/CreateModuleTest.java
@@ -3,11 +3,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CreateModuleTest {
+
     @Test
-    void createEmptyModuleTest() {
+    void createEmptyModuleTest1() {
         Module module = new Module("NAMETWO");
         assertEquals(module.getTestText(1),
                 "module NAMETWO_test_1(output out);\n" +
+                        "endmodule\n");
+    }
+
+    @Test
+    void createEmptyModuleTest2() {
+        Module module = new Module("NAMETWO");
+        assertEquals(module.getTestText(2),
+                "module NAMETWO_test_2(output out);\n" +
                         "endmodule\n");
     }
 }
