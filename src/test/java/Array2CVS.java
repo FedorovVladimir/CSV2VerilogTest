afbda6496c9f131d9f452e77fde037f5504c4fc3
Vladimir <https://github.com/FedorovVladimir/Write_Java>
2019-06-05 23:19:47 +0700

    Тест на создание CVS

---
+++ src/test/java/Array2CVS.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Array2CVS {
+
+    @Test
+    void createHeadAndOneRow() {
+        String[] headers = {
+                "a",
+        };
+
+        int[][] rows = {
+                {0},
+        };
+
+        ArrayToCVS arrayToCVS = new ArrayToCVS();
+        arrayToCVS.setHeaders(headers);
+        arrayToCVS.setRows(rows);
+
+        assertEquals("a\n" +
+                "0\n", arrayToCVS.getText());
+    }
+}
